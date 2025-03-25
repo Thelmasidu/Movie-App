352eaa8cbdafa4959a97877506366237fe06f0a2
Thelmasidu <20109631@mail.wit.ie>
2025-03-25 18:53:16 +0000

    Added routing and live data from API.

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -15,6 +15,7 @@ export interface BaseMovieProps {
     revenue: number;
     vote_count: number;
     favourite?: boolean;  
+    genre_ids?: number[];
   }
 
   export interface BaseMovieListProps { 
@@ -47,3 +48,4 @@ export interface BaseMovieProps {
     images: MovieImage[];
   }
   
+  export type FilterOption = "title" | "genre";
