2d8c02c6dd12c7c4bafe3af3bc800ae2b098ab6b
Thelmasidu <20109631@mail.wit.ie>
2025-03-27 16:31:46 +0000

    Add to favourites logic completed; Dummy favourites page.

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -27,6 +27,13 @@ const styles = {
 
     const genreId = Number(genreFilter);
 
+    const addToFavourites = (movieId: number) => {
+      const updatedMovies = movies.map((m: BaseMovieProps) =>
+        m.id === movieId ? { ...m, favourite: true } : m
+      );
+      setMovies(updatedMovies);
+    };
+
     let displayedMovies = movies
     .filter((m: BaseMovieProps) => {
       return m.title.toLowerCase().search(titleFilter.toLowerCase()) !== -1;
@@ -62,7 +69,7 @@ const styles = {
           <Header title={"Home Page"} />
         </Grid>
         <Grid item container spacing={5}>
-          <MovieList movies={displayedMovies}></MovieList>
+        <MovieList movies={displayedMovies} selectFavourite={addToFavourites} />
         </Grid>
       </Grid>
       <Fab
