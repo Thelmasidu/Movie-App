3841cfea2755afb4b67e1915e98a005c63ed6766
Thelmasidu <20109631@mail.wit.ie>
2025-04-18 21:05:42 +0100

    Add new review support.

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,7 @@ import UpcomingMoviesPage from "./pages/upComingMoviesPage";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools';
 import MoviesContextProvider from "./contexts/moviesContext";
+import AddMovieReviewPage from './pages/addMovieReviewPage';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -33,7 +34,9 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<Navigate to="/" />} />
         <Route path="/reviews/:id" element={<MovieReviewPage/>} />
+        <Route path="/reviews/form" element={<AddMovieReviewPage/>} />
         <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
+        
       </Routes>
       </MoviesContextProvider>
     </BrowserRouter>
--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -76,4 +76,12 @@ export interface BaseMovieProps {
     results: BaseMovieProps[];
   }
 
- 
+  export interface Review {
+    author: string,
+    content: string,
+    agree: boolean,
+    rating: number,
+    movieId: number,
+  }
+
+
