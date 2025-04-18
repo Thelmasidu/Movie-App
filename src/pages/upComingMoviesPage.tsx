1ce82fbd5c16d3033c27ed618c61d6e34498f831
Thelmasidu <20109631@mail.wit.ie>
2025-04-18 21:47:08 +0100

    Change upcoming movies page to use caching.

--- src/pages/upComingMoviesPage.tsx
+++ src/pages/upComingMoviesPage.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 import PageTemplate from "../components/templateMovieListPage";
 import { getUpcomingMovies } from "../api/tmdb-api";
 import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
@@ -11,22 +12,23 @@ const AddToMustWatchIcon: React.FC<{ movie: BaseMovieProps }> = ({ movie }) => {
 };
 
 const UpcomingMoviesPage: React.FC = () => {
-  const [movies, setMovies] = useState<BaseMovieProps[]>([]);
-  const [loading, setLoading] = useState(true);
+  const { data, isLoading, isError, error } = useQuery(
+    "upcomingMovies", 
+    getUpcomingMovies, 
+    {
+      staleTime: 1000 * 60 * 5, 
+      cacheTime: 1000 * 60 * 10, 
+    }
+  );
 
-  useEffect(() => {
-    getUpcomingMovies().then((data) => {
-      setMovies(data.results);
-      setLoading(false);
-    });
-  }, []);
+  
+  if (isLoading) return <h2>Loading...</h2>;
+  if (isError) return <h2>Error: {(error as Error).message}</h2>;
 
-  return loading ? (
-    <h2>Loading...</h2>
-  ) : (
+  return  (
     <PageTemplate
       title="Upcoming Movies"
-      movies={movies}
+      movies={data.results}
       action={(movie) => <AddToMustWatchIcon movie={movie} />}
     />
   );
