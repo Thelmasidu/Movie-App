816a031a10963395ade883712f846fc618123fe9
Thelmasidu <20109631@mail.wit.ie>
2025-04-18 21:35:49 +0100

    New button for Upcoming movies page.

--- src/pages/upComingMoviesPage.tsx
+++ src/pages/upComingMoviesPage.tsx
@@ -1,9 +1,17 @@
-import React, { useEffect, useState } from "react";  
+import React, { useEffect, useState } from "react";
 import PageTemplate from "../components/templateMovieListPage";
 import { getUpcomingMovies } from "../api/tmdb-api";
+import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
+import { BaseMovieProps  } from "../types/interfaces"; 
+
+const AddToMustWatchIcon: React.FC<{ movie: BaseMovieProps }> = ({ movie }) => {
+  return (
+    <PlaylistAddIcon color="primary" fontSize="large" />
+  );
+};
 
 const UpcomingMoviesPage: React.FC = () => {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState<BaseMovieProps[]>([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -17,10 +25,10 @@ const UpcomingMoviesPage: React.FC = () => {
     <h2>Loading...</h2>
   ) : (
     <PageTemplate
-  title="Upcoming Movies"
-  movies={movies}
-  selectFavourite={() => {}}
-/>
+      title="Upcoming Movies"
+      movies={movies}
+      action={(movie) => <AddToMustWatchIcon movie={movie} />}
+    />
   );
 };
 
