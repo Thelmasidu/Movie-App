253d62e04f7115fa490a77eb5a749f37bed7d1a3
Thelmasidu <20109631@mail.wit.ie>
2025-04-25 19:29:12 +0100

    Add pagination and sorting to the homepage
    Updated the homepage so users can now move between different pages of movies and sort the movie list by things like popularity or release date. Also connected the filters, sorting, and pagination together for a smoother browsing experience. The page is now way more dynamic and user-friendly!

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PageTemplate from "../components/templateMovieListPage";
 import { getMovies } from "../api/tmdb-api";
 import useFiltering from "../hooks/useFiltering";
@@ -6,10 +6,12 @@ import MovieFilterUI, {
   titleFilter,
   genreFilter,
 } from "../components/movieFilterUI";
-import { DiscoverMovies, BaseMovieProps  } from "../types/interfaces";
+import { DiscoverMovies, BaseMovieProps } from "../types/interfaces";
 import { useQuery } from "react-query";
 import Spinner from "../components/spinner";
-import AddToFavouritesIcon from '../components/cardIcons/addToFavourites'
+import AddToFavouritesIcon from "../components/cardIcons/addToFavourites";
+import { Box, Typography } from "@mui/material";
+import Pagination from "../components/pagination";
 
 const titleFiltering = {
   name: "title",
@@ -23,10 +25,19 @@ const genreFiltering = {
 };
 
 const HomePage: React.FC = () => {
-  const { data, error, isLoading, isError } = useQuery<DiscoverMovies, Error>("discover", getMovies);
-  const { filterValues, setFilterValues, filterFunction } = useFiltering(
-    [titleFiltering, genreFiltering]
+  const [page, setPage] = useState(1);
+  const [sortBy, setSortBy] = useState<string>("popularity.desc");
+
+  const { data, error, isLoading, isError } = useQuery<DiscoverMovies, Error>(
+    ["discover", page, sortBy],
+    () => getMovies(page, sortBy),
+    { keepPreviousData: true }
   );
+
+  const { filterValues, setFilterValues, filterFunction } = useFiltering([
+    titleFiltering,
+    genreFiltering,
+  ]);
 
   if (isLoading) {
     return <Spinner />;
@@ -35,7 +46,6 @@ const HomePage: React.FC = () => {
   if (isError) {
     return <h1>{error.message}</h1>;
   }
-
 
   const changeFilterValues = (type: string, value: string) => {
     const changedFilter = { name: type, value: value };
@@ -46,29 +56,45 @@ const HomePage: React.FC = () => {
     setFilterValues(updatedFilterSet);
   };
 
+  const sortChangeHandler = (newSort: string): void => {
+    setSortBy(newSort);
+  };
+
   const movies = data ? data.results : [];
   const displayedMovies = filterFunction(movies);
 
-  // Redundant, but necessary to avoid app crashing.
-  // const favourites = movies.filter(m => m.favourite)
-  // localStorage.setItem("favourites", JSON.stringify(favourites));
-  // const addToFavourites = (movieId: number) => true;
-
   return (
     <>
-       <PageTemplate
+      <PageTemplate
         title="Discover Movies"
         movies={displayedMovies}
-        action={(movie: BaseMovieProps) => {
-          return <AddToFavouritesIcon {...movie} />
-        }}
+        page={page}
+        totalPages={data?.total_pages}
+        onPrevious={() => setPage((prev) => Math.max(prev - 1, 1))}
+        onNext={() => setPage((prev) => prev + 1)}
+        action={(movie: BaseMovieProps) => <AddToFavouritesIcon {...movie} />}
       />
+
+      {/* Filters */}
       <MovieFilterUI
         onFilterValuesChange={changeFilterValues}
         titleFilter={filterValues[0].value}
         genreFilter={filterValues[1].value}
+        onSortChange={sortChangeHandler}
+        currentSort={sortBy}
       />
+
+      {/* Pagination */}
+      <Box display="flex" justifyContent="center" alignItems="center" mt={3}>
+        <Pagination
+          currentPage={page}
+          totalPages={data ? data.total_pages : 1}
+          onPageChange={setPage}
+        />
+        <Typography sx={{ ml: 2 }}>Page {page}</Typography>
+      </Box>
     </>
   );
 };
+
 export default HomePage;
