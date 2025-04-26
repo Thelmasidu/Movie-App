39b93264a02f873ea013bd31c76f902ac007e588
Thelmasidu <20109631@mail.wit.ie>
2025-04-26 20:11:42 +0100

    Add "Remove from Favorites" button for TV shows
    Created a button that lets users remove TV shows from their favorites list. Clicking the delete icon triggers the removal action from the context.

---
+++ src/components/cardIcons/removeShowsFromFavourites.tsx
@@ -0,0 +1,22 @@
+import React, { MouseEvent, useContext } from "react";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { MoviesContext } from "../../contexts/moviesContext";
+import { BaseTvShowProps } from "../../types/interfaces";
+
+const RemoveShowsFromFavouritesIcon: React.FC<BaseTvShowProps> = (show) => {
+  const context = useContext(MoviesContext);
+
+  const onUserRequest = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    context.removeShowsFromFavourites(show);
+  };
+
+  return (
+    <IconButton aria-label="remove from favorites" onClick={onUserRequest}>
+      <DeleteIcon color="primary" fontSize="large" />
+    </IconButton>
+  );
+};
+
+export default RemoveShowsFromFavouritesIcon;
