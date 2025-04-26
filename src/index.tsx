8c7360d5e60808b1465ca2e4d6b72b6ea77ea318
Thelmasidu <20109631@mail.wit.ie>
2025-04-26 16:03:29 +0100

    Add TV Shows page route to the app
    Linked the new TV Shows page to the app's routes. Now users can visit /tv-shows to see TV shows. Kept all other routes like movies and reviews working as before.

--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,7 @@ import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools';
 import MoviesContextProvider from "./contexts/moviesContext";
 import AddMovieReviewPage from './pages/addMovieReviewPage';
+import TvShowsPage from "./pages/tvShowsPage";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -33,6 +34,7 @@ const App = () => {
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<Navigate to="/" />} />
+        <Route path="/tv-shows" element={<TvShowsPage />} />
         <Route path="/reviews/:id" element={<MovieReviewPage/>} />
         <Route path="/reviews/form" element={<AddMovieReviewPage/>} />
         <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
