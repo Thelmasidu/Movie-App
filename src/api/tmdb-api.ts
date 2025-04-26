a0054d74828d17820c3fd2a49caac60e17b649b6
Thelmasidu <20109631@mail.wit.ie>
2025-04-26 18:02:03 +0100

    Add function to fetch TV show details by ID
    Created a function to fetch detailed information about a TV show from the API using its ID. Includes error handling if the fetch fails.

--- src/api/tmdb-api.ts
+++ src/api/tmdb-api.ts
@@ -119,3 +119,21 @@ export const fetchTvShowImages = (id: string | number) => {
     });
 };
 
+export const fetchTvShowDetail = (id: string) => {
+  return fetch(
+    `https://api.themoviedb.org/3/tv/${id}?api_key=${
+      import.meta.env.VITE_TMDB_KEY
+    }&language=en-US&include_adult=false&include_video=false&page=1`
+  )
+    .then((response) => {
+      if (!response.ok)
+        throw new Error(
+          `Unable to fetch tv show detail. Response status: ${response.status}`
+        );
+      return response.json();
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
+
