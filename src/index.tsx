1356efc7cd5c26f41c932d54407377ee29a449cd
Thelmasidu <20109631@mail.wit.ie>
2025-04-29 21:53:05 +0100

    Refactor routing and add SearchPage route to main app
    - Cleaned up and organized imports, removing duplicates.
    
    - Added /movies/search route to support multi-search functionality.
    
    - Wrapped the app with QueryClientProvider and MoviesContextProvider.
    
    - Integrated ReactQueryDevtools for query debugging.
    
    - Minor formatting improvements for consistency.

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,28 @@
+/* eslint-disable react-refresh/only-export-components */
 import React from "react";
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
+import TvShowsPage from "./pages/tvShowsPage";
 import MoviePage from "./pages/movieDetailsPage";
-import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; 
+import PeoplePage from "./pages/peoplePage";
+import SearchPage from "./pages/searchInputPage";
+import TvShowPage from "./pages/tvShowDetailPage";
+import FavouriteMoviesPage from "./pages/favouriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
-import SiteHeader from './components/siteHeader';
+import SiteHeader from "./components/siteHeader";
 import UpcomingMoviesPage from "./pages/upComingMoviesPage";
 import { QueryClientProvider, QueryClient } from "react-query";
-import { ReactQueryDevtools } from 'react-query/devtools';
+import { ReactQueryDevtools } from "react-query/devtools";
 import MoviesContextProvider from "./contexts/moviesContext";
-import AddMovieReviewPage from './pages/addMovieReviewPage';
-import TvShowsPage from "./pages/tvShowsPage";
-import TvShowPage from "./pages/tvShowDetailPage";
-import PeoplePage from "./pages/peoplePage";
+import AddMovieReviewPage from "./pages/addMovieReviewPage";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 360000,
-      refetchInterval: 360000, 
-      refetchOnWindowFocus: false
+      refetchInterval: 360000,
+      refetchOnWindowFocus: false,
     },
   },
 });
@@ -28,31 +30,34 @@ const queryClient = new QueryClient({
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <SiteHeader />
+      <BrowserRouter>
+        <SiteHeader />
         <MoviesContextProvider>
-      <Routes>
-        <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
-        <Route path="/movies/:id" element={<MoviePage />} />
-        <Route path="/tv-shows/:id" element={<TvShowPage />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="*" element={<Navigate to="/" />} />
-        <Route path="/tv-shows" element={<TvShowsPage />} />
-        <Route path="/reviews/:id" element={<MovieReviewPage/>} />
-        <Route path="/reviews/form" element={<AddMovieReviewPage/>} />
-        <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
-        <Route path="/people" element={<PeoplePage />} />
-      </Routes>
-      </MoviesContextProvider>
-    </BrowserRouter>
-    <ReactQueryDevtools initialIsOpen={false} />
+          <Routes>
+            <Route
+              path="/movies/favourites"
+              element={<FavouriteMoviesPage />}
+            />
+            <Route path="/movies/:id" element={<MoviePage />} />
+            <Route path="/tv-shows/:id" element={<TvShowPage />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/tv-shows" element={<TvShowsPage />} />
+            <Route path="*" element={<Navigate to="/" />} />
+            <Route path="/reviews/:id" element={<MovieReviewPage />} />
+            <Route path="/reviews/form" element={<AddMovieReviewPage />} />
+            <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
+            <Route path="/movies/search" element={<SearchPage />} />
+            <Route path="/people" element={<PeoplePage />} />
+          </Routes>
+        </MoviesContextProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 };
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
-
+  </React.StrictMode>
+);
