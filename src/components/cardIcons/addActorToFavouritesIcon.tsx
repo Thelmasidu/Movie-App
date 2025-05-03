7383c9acda686b303c228ed38855cb5dacf2a3bc
Thelmasidu <20109631@mail.wit.ie>
2025-05-03 19:43:11 +0100

    Removed Old “Add Person to Favourites” Button
    Deleted the outdated AddPersonToFavouritesIcon component, which was tied to the old "People" setup. A new version tailored for "Actors" is now being used, which matches the updated naming and structure. This keeps everything consistent and avoids confusion.

---
+++ src/components/cardIcons/addActorToFavouritesIcon.tsx
@@ -0,0 +1,27 @@
+import React, { MouseEvent, useContext } from "react";
+import { MoviesContext } from "../../contexts/moviesContext";
+import IconButton from "@mui/material/IconButton";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { Actors } from "../../types/interfaces";
+import { Tooltip } from "@mui/material";
+
+const AddPersonToFavouritesIcon: React.FC<Actors> = (actor) => {
+  const context = useContext(MoviesContext);
+
+  const onUserSelect = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    context.addActorsToFavourite(actor);
+  };
+  return (
+    <Tooltip title="Add Person to favorites">
+      <IconButton aria-label="Add to favorites" onClick={onUserSelect}>
+        <FavoriteIcon
+          color={actor.favourite ? "error" : "primary"}
+          fontSize="large"
+        />
+      </IconButton>
+    </Tooltip>
+  );
+};
+
+export default AddPersonToFavouritesIcon;
