4ebd7cad0b164c6943a701140a84093129c20295
Thelmasidu <20109631@mail.wit.ie>
2025-05-03 19:33:46 +0100

    Added New Page to Show List of Popular Actors
    Created a new page that displays a list of popular actors using data from the movie database. It includes a loading spinner, error message handling, and pagination so users can move through different pages of actors. Each actor also has a button to add them to favourites. This makes it easier for users to browse and save their favorite stars.

---
+++ src/pages/actorsPage.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { useState } from "react";
+import { useQuery } from "react-query";
+import { fetchActors } from "../api/tmdb-api";
+import { DiscoverActors, Actors } from "../types/interfaces";
+
+import PageTemplate from "../components/templateActorsListPage";
+import Spinner from "../components/spinner";
+import Pagination from "../components/pagination";
+import AddPersonToFavouritesIcon from "../components/cardIcons/AddActorToFavouritesIcon";
+
+const ActorsPage: React.FC = () => {
+  const [page, setPage] = useState(1);
+  const { data, error, isLoading, isError } = useQuery<DiscoverActors, Error>(
+    ["actors", page],
+    () => fetchActors(page),
+    { keepPreviousData: true }
+  );
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
+
+  const actors = data ? data.results : [];
+
+  return (
+    <div>
+      <PageTemplate
+        actors={actors}
+        title="Actors List"
+        page={page}
+        totalPages={data ? data.total_pages : 1}
+        action={(actor: Actors) => <AddPersonToFavouritesIcon {...actor} />}
+        onPrevious={() => setPage((prev) => Math.max(prev - 1, 1))}
+        onNext={() =>
+          setPage((prev) => (data && prev < data.total_pages ? prev + 1 : prev))
+        }
+      />
+      <Pagination
+        currentPage={page}
+        totalPages={data ? data.total_pages : 1}
+        onPageChange={setPage}
+      />
+    </div>
+  );
+};
+
+export default ActorsPage;
