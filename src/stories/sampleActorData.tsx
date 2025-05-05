730942ae4878a7b19115fb34eedb336f0c7b2fd9
Thelmasidu <20109631@mail.wit.ie>
2025-05-05 12:20:28 +0100

    Add sample actor and actorDetails mock data for testing and Storybook
    Created a mock data file containing sample actor information for testing and Storybook purposes. The default export includes a simplified Actors object (e.g., Brad Pitt), while the actorDetails array provides detailed profiles for Florence Pugh and Pedro Pascal. This enables realistic UI previews and interaction testing for components that rely on actor data.

---
+++ src/stories/sampleActorData.tsx
@@ -0,0 +1,72 @@
+import { ActorsDetails } from "../types/interfaces";
+
+export default {
+  adult: false,
+  gender: 2,
+  id: 287,
+  known_for: [
+    {
+      adult: false,
+      backdrop_path: "/fCayJrkfRaCRCTh8GqN30f8oyQF.jpg",
+      genre_ids: [18],
+      id: 550,
+      media_type: "movie",
+      original_language: "en",
+      original_title: "Fight Club",
+      overview:
+        "A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy.",
+      poster_path: "/bptfVGEQuv6vDTIMVCHjJ9Dz8PX.jpg",
+      release_date: "1999-10-15",
+      title: "Fight Club",
+      video: false,
+      vote_average: 8.4,
+      vote_count: 3439,
+      popularity: 60.441,
+    },
+  ],
+  known_for_department: "Acting",
+  name: "Brad Pitt",
+  original_name: "Brad Pitt",
+  popularity: 10.0,
+  profile_path: "/kU3B75TyRiCgE270EyZnHjfivoq.jpg",
+  favourite: true,
+};
+
+export const actorDetails: ActorsDetails[] = [
+  {
+    adult: false,
+    also_known_as: ["Florence Rose C. M. Pugh"],
+    biography:
+      "Florence Pugh is an English actress born on January 3, 1996, in Oxford, England...",
+    birthday: "1996-01-03",
+    deathday: null,
+    gender: 1,
+    homepage: null,
+    id: 1397778,
+    imdb_id: "nm6073955",
+    known_for_department: "Acting",
+    name: "Florence Pugh",
+    place_of_birth: "Oxford, Oxfordshire, England, UK",
+    popularity: 22.134,
+    profile_path:
+      "https://upload.wikimedia.org/wikipedia/commons/7/7b/Florence_Pugh_2023.jpg",
+  },
+  {
+    adult: false,
+    also_known_as: ["José Pedro Balmaceda Pascal"],
+    biography:
+      "Pedro Pascal is a Chilean-American actor born on April 2, 1975...",
+    birthday: "1975-04-02",
+    deathday: null,
+    gender: 2,
+    homepage: null,
+    id: 1253360,
+    imdb_id: "nm0050959",
+    known_for_department: "Acting",
+    name: "Pedro Pascal",
+    place_of_birth: "Santiago, Chile",
+    popularity: 28.572,
+    profile_path:
+      "https://upload.wikimedia.org/wikipedia/commons/e/e6/Pedro_Pascal_by_Gage_Skidmore_2.jpg",
+  },
+];
