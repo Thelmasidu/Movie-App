17c8a2af9df59140d6509a1296230436a858175a
Thelmasidu <20109631@mail.wit.ie>
2025-05-10 17:53:51 +0100

    Add login/signup pages and fantasy movie features to the app
    - Set up routes for login and sign-up pages so users can create accounts or log in.
    
    - Added new fantasy movie components and routes, allowing users to create and view their own fantasy movies.
    
    - Cleaned up some duplicate imports and ensured all pages are properly linked in the app navigation.
    
    - Default route now leads to the login page instead of the home page.

--- src/index.tsx
+++ src/index.tsx
@@ -19,13 +19,16 @@ import ActorDetailsPage from "./pages/actorDetailsPage";
 import TvShowsContextProvider from "./contexts/tvShowsContent";
 import SimilarMoviesPage from "./pages/similarMoviesPage";
 import ThemeContextProvider from "./contexts/themeContext";
-import StorybookSupportPage from "./pages/storyBookSupportPage"; 
+import StorybookSupportPage from "./pages/storyBookSupportPage";
 import LoginPage from "./pages/logInPage";
 import SignUpPage from "./pages/signUpPage";
+
+// 🆕 Fantasy Movie components
 import CreateFantasyMovie from "./components/fantasyMovie/createFantasyMovie";
 import FantasyMovieList from "./components/fantasyMovie/fantasyMovieDetail";
 import FantasyMovieDetail from "./components/fantasyMovie/fantasyMovieList";
 
+// ✅ React Query client setup
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -45,7 +48,10 @@ const App = () => {
           <MoviesContextProvider>
             <TvShowsContextProvider>
               <Routes>
-                <Route path="/" element={<HomePage />} />
+                {/* Core Routes */}
+                <Route path="/" element={<LoginPage />} />
+                <Route path="/signup" element={<SignUpPage />} />
+                <Route path="/home" element={<HomePage />} />
                 <Route path="/tv-shows" element={<TvShowsPage />} />
                 <Route path="/tv-shows/:id" element={<TvShowPage />} />
                 <Route path="/movies/:id" element={<MoviePage />} />
@@ -53,20 +59,24 @@ const App = () => {
                 <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
                 <Route path="/movies/search" element={<SearchPage />} />
                 <Route path="/movies/similar-movies/:id" element={<SimilarMoviesPage />} />
+
+                {/* Reviews */}
                 <Route path="/reviews/form" element={<AddMovieReviewPage />} />
                 <Route path="/reviews/:id" element={<MovieReviewPage />} />
+
+                {/* Actor Pages */}
                 <Route path="/actors" element={<ActorsPage />} />
                 <Route path="/actors/:id" element={<ActorDetailsPage />} />
-                 <Route path="/storybook/support" element={<StorybookSupportPage />} />
-                    <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
 
-                {/* 🆕 Fantasy Movie Routes */}
-                <Route path="/fantasy/create" element={<CreateFantasyMovie />} />
+                {/* Support */}
+                <Route path="/storybook/support" element={<StorybookSupportPage />} />
+
+                {/* Fantasy Movies */}
                 <Route path="/fantasy" element={<FantasyMovieList />} />
+                <Route path="/fantasy/create" element={<CreateFantasyMovie />} />
                 <Route path="/fantasy/:id" element={<FantasyMovieDetail />} />
 
-
+                {/* Redirect unmatched routes */}
                 <Route path="*" element={<Navigate to="/" />} />
               </Routes>
             </TvShowsContextProvider>
