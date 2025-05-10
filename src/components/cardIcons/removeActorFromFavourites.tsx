7466ce0b10b25dae04fe0f8fba70b0cb8204ca78
Thelmasidu <20109631@mail.wit.ie>
2025-05-10 13:48:21 +0100

    Fix: Switch to Correct Context for Removing Favourite Actor
    Updated the code to use the correct context when removing an actor from favourites. It was mistakenly using the movies context before, but now it properly uses the actors context to manage favourite actors.

--- src/components/cardIcons/removeActorFromFavourites.tsx
+++ src/components/cardIcons/removeActorFromFavourites.tsx
@@ -2,10 +2,11 @@ import React, { MouseEvent, useContext } from "react";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { MoviesContext } from "../../contexts/moviesContext";
+import { ActorsContext } from "../../contexts/actorsContext";
 import { Actors } from "../../types/interfaces";
 
 const RemovePersonFromFavouritesIcon: React.FC<Actors> = (actor) => {
-  const context = useContext(MoviesContext);
+  const context = useContext(ActorsContext);
 
   const onUserRequest = (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
