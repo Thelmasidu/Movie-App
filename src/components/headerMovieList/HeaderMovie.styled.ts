1e87ae28e952f445d663da828f9b22c303f27c3d
Thelmasidu <20109631@mail.wit.ie>
2025-05-10 13:49:43 +0100

    Remove Unused StyledHeader Export
    Deleted the styled component StyledHeader since it’s no longer being used. This helps keep the codebase clean and free of unnecessary code.

--- src/components/headerMovieList/HeaderMovie.styled.ts
+++ src/components/headerMovieList/HeaderMovie.styled.ts
@@ -1,6 +1,5 @@
 import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
-
 export const StyledHeader = styled(Paper)(({ theme }) => ({
   display: "flex",
   justifyContent: "space-between",
